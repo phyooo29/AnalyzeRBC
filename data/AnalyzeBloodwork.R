ba51747c889e6c6b866fa96023b9db7879e0c6ca
phyooo29 <56736874+phyooo29@users.noreply.github.com>
2019-11-11 10:50:33 -0500

    Update AnalyzeBloodwork.R

--- data/AnalyzeBloodwork.R
+++ data/AnalyzeBloodwork.R
@@ -1,41 +1,79 @@
-## BTEC330 Robinson Project2
-
+## BTEC330 F2019 Project2 Robinson
 
 ## Install necessary packages
-
 install.packages("ggplot2")
 library(ggplot2)
-library(ggplot2)
-
-install.packages("scatterplot3d")
-library(scatterplot3d)
 
 ## Read data
-IBS1 <- read.csv("RobinsonEtAl_Sup1.csv", header = TRUE)
+IBS1 <- read.csv("data/RobinsonEtAl_Sup1.csv", header = TRUE)
 head(IBS1)
-write.csv(IBS1, "data_output.csv")
 
-##  Single Regressions for BMI vs. each blood data variable
-##  Data was obtained from Robinson, et al. 2019 (doi: https://doi.org/10.1101/608208)
+IBS1$RBC_result <- "NA"
+
+## Assign "HIGH", "NORMAL", or "LOW" based on clinical range to the RBC_result parameter
+
+IBS1$RBC_result[IBS1$RBC > 6.1] <- "HIGH"
+
+IBS1$RBC_result[IBS1$RBC <= 6.1 & IBS1$RBC >= 4.6] <- "NORMAL"
+
+IBS1$RBC_result[IBS1$RBC < 4.6] <- "LOW"
+
+write.csv(IBS1, "data_output/RBC.csv")
+
+##  Single Regressions 
+##  Data obtained from Robinson, et al. 2019 (doi: https://doi.org/10.1101/608208)
 ##  https://statquest.org/2017/10/30/statquest-multiple-regression-in-r/
 ##  http://www.sthda.com/english/articles/40-regression-analysis/167-simple-linear-regression-in-r/
 ##  http://r-statistics.co/Linear-Regression.html
 
-## Single Regression Test
-single.regression <- lm(BMI ~ RBC, data=IBS1)
-summary(single.regression)
+## Single Regression Test, BMI vs. Bloodwork parameter
+RBC.regression <- lm(BMI ~ RBC, data = IBS1)
+summary(RBC.regression)
 
+## Output the results to a file
+## http://www.cookbook-r.com/Data_input_and_output/Writing_text_and_output_from_analyses_to_a_file/
+sink('data_output/LDH_regression.txt', append = TRUE)
+print(RBC.regression)
+sink()
 
+## ANOVA: IBS1-subtype vs. Bloodwork parameter
+## http://www.sthda.com/english/wiki/one-way-anova-test-in-r
+RBC.aov <- aov(RBC ~ IBS.subtype, data = IBS1)
+summary(RBC.aov)
+sink('data_output/LDH_anova.txt', append = TRUE)
+print(RBC.aov)
+sink()
+
+## Print scatterplot and box plots as .png files into "fig_output" project directory.
+## http://www.sthda.com/english/wiki/ggsave-save-a-ggplot-r-software-and-data-visualization
 
 ## Scatterplots
 ## https://www.statmethods.net/graphs/scatterplot.html
 
-
-ggplot(IBS1, aes(x=BMI, y=RBC)) +
+ggplot(IBS1, aes(x = BMI, y = RBC)) +
   geom_point() +    
-  geom_smooth(method=lm) 
+  geom_smooth(method = lm) 
 
+png("fig_output/RBC_scatterplot.png")
+RBC_scatterplot <- ggplot(IBS1, aes(x = BMI, y = RBC)) +
+  geom_point() +    
+  geom_smooth(method = lm) 
 
+print(RBC_scatterplot)
+dev.off()
 
+## Box plots
+## https://www.statmethods.net/graphs/boxplot.html
+
+boxplot(RBC ~ IBS.subtype, data = IBS1, main="RBC by IBS subtype", 
+        xlab = "IBS.subtype", ylab = "RBC"
+)
+
+png("fig_output/RBC_boxplot.png")
+RBC_boxplot <- boxplot(RBC ~ IBS.subtype, data = IBS1, main="RBC by IBS subtype", 
+                       xlab = "IBS.subtype", ylab = "RBC"
+)
+print(RBC_boxplot)
+dev.off()
 
 
